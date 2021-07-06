665f43d55b3323152723024e2a096574d0028b1a
Ryaba <kirya.davidovich.06@mail.ru>
2021-07-06 19:14:21 +0300

    ref spawn images

---
+++ scripts/interfaces.ts
@@ -0,0 +1,10 @@
+export interface ICard {
+    imageContainer: HTMLElement | null;
+    cover: HTMLElement | null;
+    imageId: number | undefined;
+}
+
+export interface IImage {
+    image: string;
+    id: number;
+}
--- ts-built/script.js
+++ ts-built/script.js
@@ -1,82 +1,8 @@
-import variables from "./variables.js";
+import { variables, cards, images } from "./variables.js";
+import { shuffle } from "./utils.js";
 if (variables.gameStateToggler !== null) {
     // Game state toggler
 }
-if (variables.registerOpen !== null) {
-    variables.registerOpen.onclick = function () {
-        variables.registerWindow.classList.toggle("register-visible");
-        variables.substrate.classList.remove("substrate-hidden");
-        variables.substrate.classList.toggle("substrate-visible");
-    };
-}
-variables.registerAdd.onclick = function () {
-    function checkRegisterInputs() {
-        let input_1 = false;
-        let input_2 = false;
-        let input_3 = false;
-        if (variables.registerInput_1.value.length < 2) {
-            variables.registerIconErr_1.style.display = "flex";
-            variables.registerIconCheck_1.style.display = "none";
-        }
-        else {
-            variables.registerIconCheck_1.style.display = "unset";
-            variables.registerIconErr_1.style.display = "none";
-            input_1 = true;
-        }
-        if (variables.registerInput_2.value.length < 2) {
-            variables.registerIconErr_2.style.display = "flex";
-            variables.registerIconCheck_2.style.display = "none";
-        }
-        else {
-            variables.registerIconCheck_2.style.display = "unset";
-            variables.registerIconErr_2.style.display = "none";
-            input_2 = true;
-        }
-        if (variables.registerInput_3.value.length < 2) {
-            variables.registerIconErr_3.style.display = "flex";
-            variables.registerIconCheck_3.style.display = "none";
-        }
-        else {
-            variables.registerIconCheck_3.style.display = "unset";
-            variables.registerIconErr_3.style.display = "none";
-            input_3 = true;
-        }
-        if (input_1 && input_2 && input_3 === true) {
-            return true;
-        }
-    }
-    if (checkRegisterInputs() === true) {
-        variables.registerWindow.classList.remove("register-visible");
-        variables.substrate.classList.toggle("substrate-hidden");
-        variables.gameStateToggler.style.display = "unset";
-        variables.headerAvatar.style.display = "unset";
-        variables.registerOpen.style.display = "none";
-    }
-};
-variables.registerCancel.onclick = function () {
-    variables.registerWindow.classList.remove("register-visible");
-    variables.substrate.classList.toggle("substrate-hidden");
-};
-let avatar = "default";
-variables.registerAvatar.onchange = function () {
-    let file = variables.registerAvatar.files[0];
-    if (file) {
-        variables.registerUserAvatar.src = URL.createObjectURL(file);
-        variables.headerAvatar.src = URL.createObjectURL(file);
-        localStorage.setItem("myImage", variables.registerUserAvatar.src);
-    }
-    variables.registerUserAvatar.style.display = "unset";
-    variables.registerNoAvatar.style.display = "none";
-    avatar = "custom";
-};
-if (avatar === "default") {
-    variables.headerAvatar.src = "./assets/icons/no-avatar.svg";
-}
-variables.substrate.onclick = function () {
-    variables.substrate.classList.toggle("substrate-hidden");
-    variables.registerWindow.classList.remove("register-visible");
-    //variables.resultWindow.classList.remove("result-visible");
-};
 let gameState = "not active";
 variables.gameStateToggler.onclick = function () {
     if (gameState === "not active") {
@@ -115,97 +41,20 @@ function clocktimer() {
 }
 //------------Game----------------//
 window.onload = function () {
-    spawnImages(shuffle(imagesArr));
+    spawnImages(cards, shuffle(images));
     clocktimer();
 };
-let image_1 = `<img class="game-field__container-image" src="./assets/images/elephant.png" />`;
-let image_2 = `<img class="game-field__container-image" src="./assets/images/elephant.png" />`;
-let image_3 = `<img class="game-field__container-image" src="./assets/images/bird.png" />`;
-let image_4 = `<img class="game-field__container-image" src="./assets/images/bird.png" />`;
-let image_5 = `<img class="game-field__container-image" src="./assets/images/wolf.png" />`;
-let image_6 = `<img class="game-field__container-image" src="./assets/images/wolf.png" />`;
-let image_7 = `<img class="game-field__container-image" src="./assets/images/lion.png" />`;
-let image_8 = `<img class="game-field__container-image" src="./assets/images/lion.png" />`;
-let image_9 = `<img class="game-field__container-image" src="./assets/images/tiger.png" />`;
-let image_10 = `<img class="game-field__container-image" src="./assets/images/tiger.png" />`;
-let image_11 = `<img class="game-field__container-image" src="./assets/images/pinguin.png" />`;
-let image_12 = `<img class="game-field__container-image" src="./assets/images/pinguin.png" />`;
-let imagesArr = [image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8, image_9, image_10, image_11, image_12];
-function shuffle(arr) {
-    return arr.sort(() => Math.random() - 0.5);
+function spawnImages(cards, images) {
+    cards.forEach((card, i) => {
+        card.imageContainer.innerHTML = images[i].image;
+        card.imageId = images[i].id;
+        addOnclicks(card);
+    });
 }
-function spawnImages(arr) {
-    variables.gameImage_1.innerHTML = arr[0];
-    variables.gameImage_2.innerHTML = arr[1];
-    variables.gameImage_3.innerHTML = arr[2];
-    variables.gameImage_4.innerHTML = arr[3];
-    variables.gameImage_5.innerHTML = arr[4];
-    variables.gameImage_6.innerHTML = arr[5];
-    variables.gameImage_7.innerHTML = arr[6];
-    variables.gameImage_8.innerHTML = arr[7];
-    variables.gameImage_9.innerHTML = arr[8];
-    variables.gameImage_10.innerHTML = arr[9];
-    variables.gameImage_11.innerHTML = arr[10];
-    variables.gameImage_12.innerHTML = arr[11];
+function addOnclicks(card) {
+    card.cover.onclick = () => {
+        console.log(card.imageId);
+        card.imageContainer.style.display = "unset";
+        card.cover.style.display = "none";
+    };
 }
-let imageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-imageNumbers.forEach(function (i) {
-    variables.gameImage_1.onclick = function () {
-        variables.gameImage_1.style.display = "none";
-        variables.backsideCard_1.style.display = "unset";
-    };
-    variables.backsideCard_1.onclick = function () {
-        variables.gameImage_1.style.display = "unset";
-        variables.backsideCard_1.style.display = "none";
-    };
-});
-let imageId = [
-    {
-        id: 1,
-        img: image_1,
-    },
-    {
-        id: 2,
-        img: image_2,
-    },
-    {
-        id: 3,
-        img: image_3,
-    },
-    {
-        id: 4,
-        img: image_4,
-    },
-    {
-        id: 5,
-        img: image_5,
-    },
-    {
-        id: 6,
-        img: image_6,
-    },
-    {
-        id: 7,
-        img: image_7,
-    },
-    {
-        id: 8,
-        img: image_8,
-    },
-    {
-        id: 9,
-        img: image_9,
-    },
-    {
-        id: 10,
-        img: image_10,
-    },
-    {
-        id: 11,
-        img: image_11,
-    },
-    {
-        id: 12,
-        img: image_12,
-    },
-];
