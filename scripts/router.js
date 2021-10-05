5a2a97bf905db65946868c0b9fd849968cd4a16b
Ryabaa <73956155+Ryabaa@users.noreply.github.com>
2021-10-05 10:04:02 +0300

    fix router

--- scripts/router.js
+++ scripts/router.js
@@ -3,26 +3,26 @@ import { pages } from './pages.js'
 
 
 switch (location.pathname) {
-    case '/game':
+    case '/match-match/game':
             variables.main.innerHTML = pages.game
             variables.navLinkGame.style.background = '#2F80ED'
         break;
 
-    case '/about':
+    case '/match-match/about':
             variables.main.innerHTML = 'about'
             variables.navLinkAbout.style.background = '#2F80ED'
         break;
 
-    case '/score':
+    case '/match-match/score':
             variables.main.innerHTML = 'score'
             variables.navLinkScore.style.background = '#2F80ED'
         break;
 
-    case '/settings':
+    case '/match-match/settings':
             variables.main.innerHTML = 'settings'
             variables.navLinkSettings.style.background = '#2F80ED'
         break;
 
     default:
-        location.pathname = '/game'
+        location.pathname = '/match-match/game'
 }
